6b915e2374db50f6ee19fa3b8f5c10cfff3daf29
batuhan <batuhan.tanir00@gmail.com>
2024-05-08 23:18:17 +0300

    refactor: Remove unused code and optimize loading in NotFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,29 +1,15 @@
 "use client";
-import { useTheme } from "next-themes";
 import Link from "next/link";
 import React from "react";
 
-function NotFound() {
+async function NotFound() {
   React.useEffect(() => {
     import("@lottiefiles/lottie-player");
   });
 
-  const { resolvedTheme } = useTheme();
-  console.log(resolvedTheme);
   return (
     <div className="flex flex-col gap-5 items-center justify-center w-full min-h-full flex-1 py-10">
       <div className="relative w-[200px] h-[150px] sm:w-[300px] sm:h-[230px]">
-        <lottie-player
-          autoplay
-          loop
-          src={
-            resolvedTheme == "dark" || resolvedTheme == "system"
-              ? "/black-not-found.json"
-              : "/black-not-found.json"
-          }
-          fill
-          alt="Sayfa Bulunamadı 404"
-        />
         <h2 className="sr-only">Loading</h2>
       </div>
       <div className="text-center">
