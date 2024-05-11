7b3ab60b86975f8c166a6f9c40000b68466f44e5
batuhan <batuhan.tanir00@gmail.com>
2024-05-11 20:01:41 +0300

    chore: Add font import and update loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-
 function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div className="flex items-center justify-center pt-32 h-[calc(100vh_-_399px)]">
+      <div className="text-[#002366] rounded-full text-4xl md:text-7xl whitespace-nowrap animate-pulse font-nourd">
+        We Codez
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
